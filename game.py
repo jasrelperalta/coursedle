c3b6dc1fdd19bf830f04e5219e6e6d8f82e27e61
jasrelperalta <jtperalta1@up.edu.ph>
2022-12-07 17:26:06 +0800

    followed new flowchart

--- game.py
+++ game.py
@@ -1,23 +1,63 @@
 import mechanics
-import os
-
-def clear():
-    os.system('cls' if os.name == 'nt' else 'clear')    # linux or windows
+from colorama import Back, init
 
 
+init(autoreset=True)   
 
-def playGame(n, arrAttempts, letterAttempts, guessWord):
-    clear()
+# n = number of attempts
+# arrAttempts = previous words na ininput
+# letterAttempts = list of letters na ginamit before
+# guessWord = word na huhulaan
+def playGame(n, arrAttempts, guessWord):
+    # coursedle banner with colors
     mechanics.coursedle_mechanics()
 
-    # function that prints yung mga nahulaan na
-    # function that prints yung alphabet + color red yung ginamit na before // check if already in letter attempts
+    # print prev attempts if resume
+        # padaanin sa check function
+
+    while n < 4:
+        # print # of attempts
+        # get input // get upper
+        # check
+        # update array of attempts and n
+        end = input("\n") # need 4 letters input
+        if end == guessWord:
+            winScreen()
+        check(end, guessWord)
+        n += 1
+    loseScreen()
+
+
+def winScreen():
+    print("u win lol")
+    end = input("Press ENTER to continue")
+
+def loseScreen():
+    print("lose")
+    end = input("Press ENTER to continue")
+
     
-    # function that kukuha ng input
-    # function that updates yung array of attempts and letters
-    # function that checks yung mga tamang letters ng user
-    # if tama, show win screen, then main menu
-    # elif mali but n < 4, call another instance ng playgame with updated arrays
-    # elif mali n == 4 na, show lose screen, then main menu
-    print(guessWord)
-    end = input("End")
+        
+def check(code_guess,code_random):
+    if code_guess == code_random:
+        for i in range(4):
+            print(Back.GREEN + ' ' + code_guess[i] + ' ' , end ='')
+    else:
+        for i in range(4):
+        
+            # print(code_guess[i])
+            count = code_random.count(code_guess[i])
+            # print(count)
+        
+            if count > 0:
+                #print(code_guess[i], 'exist')
+                
+                if code_guess[i] == code_random[i]:
+                    print(Back.GREEN + ' ' + code_guess[i] + ' ' , end ='')
+                else:
+                    print(Back.YELLOW + ' ' + code_guess[i] + ' ' , end ='')
+            
+            else:
+                #print(code_guess[i], 'does not exist')
+                print(Back.RED + ' ' + code_guess[i] + ' ' , end='')
+    
